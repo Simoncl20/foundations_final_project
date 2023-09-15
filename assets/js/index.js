00e277e36612b3c2306eef4371ceabbda188fdb3
Simoncl20 <simoncalderonl2010@gmail.com>
2023-09-14 22:43:46 -0500

    update print products

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
-async function getProducts(){
+async function getProducts() {
     try {
-        const data = await fetch("https://ecommercebackend.fundamentos-29.repl.co/");
+        const data = await fetch(
+            "https://ecommercebackend.fundamentos-29.repl.co/"
+        );
         const products = await data.json();
 
         window.localStorage.setItem("products", JSON.stringify(products));
 
         return products;
-
         
     } catch (error) {
         console.log(error);
     }
 }
 
-
-function printProducts(db){
+function printProducts(db) {
     const $products = document.querySelector(".products");
 
     let html = "";
@@ -22,25 +22,34 @@ function printProducts(db){
     for (const product of db.products) {
         html += `
             <div class="product">
-                <p>${product.name}</p>
+                <div class="product_img">
+                    <img src="${product.image}" alt="imagen">
+                </div>
+
+                <div className="product_info">
+                    <h3>${product.name} <span> <b>Stock</b>: ${product.quantity} </span></h3>
+                    <h4>
+                        $${product.price}
+                        <i class="bx bx-plus" id='${product.id}'></i>
+                    </h4>
+                </div>
+                
             </div>
         `;
     }
 
     $products.innerHTML = html;
-
 }
 
-
-async function main(){
+async function main() {
     const db = {
-        products: JSON.parse(window.localStorage.getItem("products")) || await getProducts(),
+        products:
+            JSON.parse(window.localStorage.getItem("products")) ||
+            (await getProducts()),
         cart: {},
-    }
-    
-    printProducts(db);
+    };
 
+    printProducts(db);
 }
 
-
-main()
+main();
